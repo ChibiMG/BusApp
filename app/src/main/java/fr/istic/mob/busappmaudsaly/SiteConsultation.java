98c2e6bada1fd8e5d9fee0962877fbfc6df4f851
MaudG <maudg35@outlook.fr>
2019-11-21 22:45:53 +0100

    modif

--- app/src/main/java/fr/istic/mob/busappmaudsaly/SiteConsultation.java
+++ app/src/main/java/fr/istic/mob/busappmaudsaly/SiteConsultation.java
@@ -1,9 +1,17 @@
 package fr.istic.mob.busappmaudsaly;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Process;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -14,6 +22,7 @@ import java.net.URL;
 import java.net.URLConnection;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 /**
  * @author Maud Garcon & Saly Knab
@@ -30,15 +39,20 @@ import androidx.annotation.Nullable;
  * TODO : service qui regarde de temps en temps si nouvelle version (dans ce cas si tel echou ou aura toujours pas la bonne version donc une nouvelle notif plus tard)
  * TODO : test : lors de l'installation on télécharge automatiquement le 1 fichier JSON (mais pas zip qui est dedans)
  * TODO : le service se lance au démarrage du téléphone
+ * TODO : appelle réseau en periodique : workmanager
  */
 
-public class SiteConsultation extends Service {
+public class SiteConsultation extends Service{
 
     private URL url;
     private String contenu;
     private BufferedReader br;
 
-    private static final int forgroundId = 55544433;
+    private String CHANNEL_ID = "FSC";
+
+    private Looper mServiceLooper;
+    private ServiceHandler mServiceHandler;
+
     private Notification notification;
 
     public SiteConsultation() {
@@ -51,7 +65,14 @@ public class SiteConsultation extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startID) {
-/*
+        createNotificationChannel();
+        notification = new NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle("Notif").build();
+        startForeground(1, notification);
+
+        Message msg = mServiceHandler.obtainMessage();
+        msg.arg1 = startID;
+        mServiceHandler.sendMessage(msg);
+
         try {
             br = new BufferedReader(new InputStreamReader(url.openStream()));
             contenu = br.readLine();
@@ -59,10 +80,9 @@ public class SiteConsultation extends Service {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        */
-        startForeground(forgroundId, notification);
+
         Toast.makeText(this, "Services ok", Toast.LENGTH_LONG).show();
-        return super.onStartCommand(intent, flags, startID);
+        return START_NOT_STICKY;
     }
 
     @Nullable
@@ -73,6 +93,45 @@ public class SiteConsultation extends Service {
 
     @Override
     public void onCreate() {
-        super.onCreate();
+        HandlerThread thead = new HandlerThread("SSA", Process.THREAD_PRIORITY_BACKGROUND);
+        thead.start();
+        mServiceLooper = thead.getLooper();
+        mServiceHandler = new ServiceHandler(mServiceLooper);
+        //super.onCreate();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+    private void createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(CHANNEL_ID, "FSC", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+
+    }
+
+    private class ServiceHandler extends Handler {
+        public ServiceHandler(Looper looper){
+            super(looper);
+        }
+
+        /*
+        @Override
+        public void handleMessage(Message msg){
+            try{
+                createNotificationChannel();
+                //Notification notification = new NotificationCompat.Builder().setContentTitle("Notif").build();
+                startForeground(1, notification);
+            }catch (){
+                Thread.currentThread().interrupt();
+            }
+            stopSelf(msg.arg1);
+        }
+
+         */
     }
 }
